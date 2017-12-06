818048dbfb00294f4054f6e35dc5ec2a57a05244
harlandos <34236779+harlandos@users.noreply.github.com>
2017-12-06 12:13:32 +0800

    Add files via upload
    Last changed file didn't update. Trying again.

--- poker_modules/table.js
+++ poker_modules/table.js
@@ -211,6 +211,8 @@ Table.prototype.initializeRound = function( changeDealer ) {
 		this.public.board = ['', '', '', '', ''];
 		this.deck.shuffle();
 		this.headsUp = this.playersSittingInCount === 2;
+		this.public.biggestBet = 0;
+		this.public.oldBiggestBet = 0;
 		this.playersInHandCount = 0;
 
 		for( var i=0 ; i<this.public.seatsCount ; i++ ) {
@@ -271,7 +273,7 @@ Table.prototype.initializeSmallBlind = function() {
 };
 
 /**
- * Method that starts the "small blind" round
+ * Method that starts the "big blind" round
  */
 Table.prototype.initializeBigBlind = function() {
 	// Set the table phase to 'bigBlind'
